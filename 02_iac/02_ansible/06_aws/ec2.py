98b5e0557bf23cd1da9405d6cd47b5d2bf7c8d63
Mariossan <mario.sanchez@ontwice.com>
2019-02-28 07:37:42 -0600

    mejoras

--- 02_iac/02_ansible/06_aws/ec2.py
+++
@@ -1,3 +0,0 @@
-export ANSIBLE_HOST_KEY_CHECKING=https://048963988660.signin.aws.amazon.com/console #Disable If a host is not initially in ‘known_hosts’ this will result in prompting for confirmation of the key, which results in an interactive experience.
-export AWS_ACCESS_KEY_ID='AKIAJP3EV7XMIU4FTSHQ' #Check Slide 5
-export AWS_SECRET_ACCESS_KEY='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' #Check Slide 5
